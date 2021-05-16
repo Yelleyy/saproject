a319dda8c763562c28c426ed407ff442fa130b5b
Yelley <72259442+yelinw@users.noreply.github.com>
2021-05-16 21:35:49 +0700

    Delete sa2 directory

--- sa2/server.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    $conn = mysqli_connect("localhost", "root", "12345678", "data") or die("Connection failed: " . mysqli_error($conn));
-    if (!$conn) {
-        die("Connection failed" . mysqli_connect_error());
-    } 
-
-?>
-<link rel="stylesheet" href="style1.css">
